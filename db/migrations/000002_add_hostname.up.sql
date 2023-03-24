5b273c5e6c366211e4bc3855c5a5b7ba346195b1
Mostafa Razavi <mostafa@sepent.com>
2023-03-24 06:10:30 +0100

    Make sure we can run migrations on an empty db
    Introducing the "not null" hostname column was failing because the seed
    rows contained a non-null value for it.

--- db/migrations/000002_add_hostname.up.sql
+++ db/migrations/000002_add_hostname.up.sql
@@ -1,4 +1,13 @@
 alter table urls
-      add column hostname text not null;
+      add column hostname text;
+
+-- set the hostnames for the seed urls, before we make it "not null"
+update urls set hostname = 'elektito.com' where url = 'gemini://elektito.com/';
+update urls set hostname = 'medusae.space' where url = 'gemini://medusae.space/index.gmi';
+update urls set hostname = 'gemini.circumlunar.space' where url = 'gemini://gemini.circumlunar.space/';
+update urls set hostname = 'gemini.conman.org' where url = 'gemini://gemini.conman.org/';
+
+alter table urls
+      alter column hostname set not null;
 
 create index urls_hostname on urls (hostname);
